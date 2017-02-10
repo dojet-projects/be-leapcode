d9f4c4e3ef910b8a704149e7cd488faa81449d5c
liyan34 <liyan34@lenovo.com>
2017-02-10 15:40:49 +0800

    runtime ubuntu16

--- config/runtime.conf.php
+++ config/runtime.conf.php
@@ -3,6 +3,7 @@ define('C_RUNTIME_LOCAL', 'runtime_local');
 define('C_RUNTIME_105', 'runtime_105');
 define('C_RUNTIME_228', 'runtime_228');
 define('C_RUNTIME_MAC2010', 'mac2010');
+define('C_RUNTIME_UBUNTU16', 'ubuntu16');
 
 $__c = &Config::configRefForKeyPath('runtime');
 
@@ -14,6 +15,8 @@ if (defined('RUNTIME')) {
     $__c = C_RUNTIME_228;
 } elseif (file_exists('/var/.iammac2010')) {
     $__c = C_RUNTIME_MAC2010;
+} elseif (file_exists('/var/.iamubuntu16')) {
+    $__c = C_RUNTIME_UBUNTU16;
 } else {
     throw new Exception("illegal runtime", 1);
 }
