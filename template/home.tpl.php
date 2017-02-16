5dffc8864e376e0b0f74d80ed0d8e56058193358
liyan34 <liyan34@lenovo.com>
2017-02-16 10:59:40 +0800

    sticky-footer

--- template/home.tpl.php
+++ template/home.tpl.php
@@ -12,6 +12,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="/static/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/static/css/sticky-footer.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
--- template/mod/footer.tpl.php
+++ template/mod/footer.tpl.php
@@ -1,6 +1,6 @@
 <footer class="footer">
   <div class="container">
     <hr />
-    <p class="text-center">Copyright &copy; 2017 Leapcode</p>
+    <p class="text-center text-muted">Copyright &copy; 2017 Leapcode</p>
   </div>
 </footer>
