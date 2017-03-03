09b36a68f2455782fbdd0fbf2cccd3259efb5d74
liyan34 <liyan34@lenovo.com>
2017-03-03 16:17:02 +0800

    aliyun leapcode conf

--- config/global.conf.php
+++ config/global.conf.php
@@ -5,7 +5,7 @@ $__c['coderoot'] = array(
     C_RUNTIME_228 => '/home/liyan34/leapcode/',
     C_RUNTIME_MAC2010 => '/Users/leeyan/github/leapcode/',
     C_RUNTIME_UBUNTU16 => '/home/lee/leapcode/',
-    C_RUNTIME_ALIYUN => '/home/lee/leapcode/',
+    C_RUNTIME_ALIYUN => '/data/www/leapcode.cn/leapcode/',
     );
 
 unset($__c);
