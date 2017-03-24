7bf47a7a603f1f58df485d302944afae9f8ce5df
liyan34 <liyan34@lenovo.com>
2017-03-23 20:15:25 -0700

    beian

--- template/mod/footer.tpl.php
+++ template/mod/footer.tpl.php
@@ -1,6 +1,9 @@
-<footer class="footer">
+<footer class="footer text-center">
   <div class="container">
     <hr />
-    <p class="text-center text-muted">Copyright &copy; 2017 Leapcode</p>
+    <p class="text-muted">
+        Copyright &copy; 2017 Leapcode
+    </p>
+    <p><small><a href="http://www.miitbeian.gov.cn/" target="_blank">京ICP备17016033号</a></small></p>
   </div>
 </footer>
