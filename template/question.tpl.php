2ca0da8bde0d3a910b1d84ff64fe70d27b500c19
liyan34 <liyan34@lenovo.com>
2017-03-31 11:30:58 +0800

    keywords

--- template/question.tpl.php
+++ template/question.tpl.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <meta name="description" content="">
+    <meta name="description" content="<?php echo sprintf("%s 难度：%d颗星", safeHtml($tpl_question['title']), $tpl_question['difficulty']); ?>">
+    <meta name="keywords" content="<?php echo sprintf("%s", safeHtml($tpl_question['title'])); ?>">
     <meta name="author" content="">
     <link rel="icon" href="/favicon.ico">
 
