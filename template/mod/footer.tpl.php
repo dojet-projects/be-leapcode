e1934fe90cd385e75b9971e4e4b314e0e33a2068
liyan34 <liyan34@lenovo.com>
2017-04-08 23:08:22 +0800

    send feedback

--- template/mod/footer.tpl.php
+++ template/mod/footer.tpl.php
@@ -7,6 +7,9 @@
     <p><small><a href="http://www.miitbeian.gov.cn/" target="_blank">京ICP备17016033号</a></small></p>
   </div>
 </footer>
+<a href="mailto:setimouse@qq.com?subject=意见与建议" id="feedback" target="_blank">
+  <i class="glyphicon glyphicon-envelope"></i> 意见与建议
+</a>
 <div style="display: none;">
 <script language="javascript" type="text/javascript" src="//js.users.51.la/19153469.js"></script>
 <noscript><a href="//www.51.la/?19153469" target="_blank"><img alt="&#x6211;&#x8981;&#x5566;&#x514D;&#x8D39;&#x7EDF;&#x8BA1;" src="//img.users.51.la/19153469.asp" style="border:none" /></a></noscript>
